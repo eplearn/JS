c4abe7ffa330d4278c70b358092aa28aba57f447
Ev Paul <git@github.com:eplearn/frontend.git>
2021-01-09 16:57:23 +0400

    working with variables

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let age = 101;
+alert('Age is ' + age);
+age++;
+alert('Next age is ' + age);
+
+/*
+let scalarVariable = 6;
+const scalarConstant = 7;
+
+const o = {scalarVariable, scalarConstant};
+console.dir(o);
+
+1!='1';
+1!=='1';
+*/
