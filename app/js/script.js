ed71bf81224b9c2c4a89a59cf6bd749d0d113469
Ev Paul <git@github.com:eplearn/frontend.git>
2021-01-10 11:56:40 +0400

    clear

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,2 @@
 'use strict';
 
-let name = 'Ivan';
-let age = 101; // объявление переменной и присвоение ей численного значения
-const increment = 1; // объявление константы и присвоение ей значения строкового типа
-alert('Age is ' + age);
-alert('Next age is ' + (age + increment));
-
-/*
-let scalarVariable = 6;
-const scalarConstant = 7;
-
-const o = {scalarVariable, scalarConstant};
-console.dir(o);
-
-1!='1';
-1!=='1';
-*/
-
